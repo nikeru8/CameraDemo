6a9d7fcb8a0972c708ed7336145334389e072450
nikeru8 <nikeru888@gmail.com>
2018-04-17 17:46:17 +0800

    new

---
+++ app/src/main/java/com/hello/kaiser/camerademo/MainActivity.java
@@ -0,0 +1,62 @@
+package com.hello.kaiser.camerademo;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAPTURE_IMAGE = 100;
+
+    private Button mButton;
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initSet();
+        initListener();
+    }
+
+    private void initSet() {
+        mButton = (Button) findViewById(R.id.button_take_pic);
+        mImageView = (ImageView) findViewById(R.id.image_view_show);
+    }
+
+    private void initListener() {
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCameraIntent();
+            }
+        });
+    }
+
+    private void openCameraIntent() {
+        Intent pictureIntent = new Intent(
+                MediaStore.ACTION_IMAGE_CAPTURE
+        );
+        if (pictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(pictureIntent,
+                    REQUEST_CAPTURE_IMAGE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == REQUEST_CAPTURE_IMAGE &&
+                resultCode == RESULT_OK) {
+            if (data != null && data.getExtras() != null) {
+                Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+                mImageView.setImageBitmap(imageBitmap);
+            }
+        }
+    }
+}
